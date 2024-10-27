eff36a1c021621212a381146a047e2b458a9939c
Sergey Spisak <serega232003@gmail.com>
2024-10-27 19:59:01 +0300

    change to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'catalog'
 
 urlpatterns = [
-    path('home/', views.show_home, name='show_home'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('product/<int:pk>', views.product_detail, name='product')
+    path('home/', views.CatalogListView.as_view(), name='show_home'),
+    path('contacts/', views.CatalogTemplateView.as_view(), name='contacts'),
+    path('product/<int:pk>', views.CatalogDetailView.as_view(), name='product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,46 +1,82 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
 from django.conf import settings
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, TemplateView
 
 from catalog.models import Product
 
 base_dir = settings.BASE_DIR
 
 
-def show_home(request: HttpRequest):
-    """Обрабатывает запрос и возвращает html-страницу"""
-    if request.method == 'GET':
-        products = Product.objects.all()
-        context = {'products': products}
-
-        return render(request, "catalog/home.html", context=context)
+class CatalogListView(ListView):
+    """Класс для представления главной страницы каталога"""
+    model = Product
+    template_name = "catalog/home.html"
+    context_object_name = "products"
 
 
-def show_contacts(request: HttpRequest):
-    """Обрабатывает запрос и возвращает html-страницу"""
-    if request.method == 'GET':
-        return render(request, "catalog/contacts.html")
+class CatalogDetailView(DetailView):
+    """Класс для представления страницы полной информации о товаре"""
+    model = Product
+    template_name = "catalog/product_detail.html"
+    context_object_name = "product"
 
 
-def contacts(request: HttpRequest):
-    """Обрабатываем форму и возвращаем ответ"""
-    if request.method == 'POST':
-        # Получение данных из формы
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
-        write_in_file = (f"\nИмя пользователя: {name}\n"
-                         f"Телефон пользователя: {phone}\n"
-                         f"Его сообщение: {message}\n")
-        with open(r"C:\Users\Amd\Desktop\SkyPro\Messages.txt", "a", encoding="utf-8") as file:
-            file.write(write_in_file)
-        # А здесь мы просто возвращаем простой ответ пользователю на сайте:
-        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(request, 'catalog/contacts.html')
+class CatalogTemplateView(TemplateView):
+    """Класс для представления страницы обратной связи"""
+    template_name = "catalog/contacts.html"
 
+    def post(self, request):
+        if self.request.method == 'POST':
+            # Получение данных из формы
+            name = request.POST.get('name')
+            phone = request.POST.get('phone')
+            message = request.POST.get('message')
+            # Обработка данных (например, сохранение в БД, отправка email и т. д.)
+            write_in_file = (f"\nИмя пользователя: {name}\n"
+                             f"Телефон пользователя: {phone}\n"
+                             f"Его сообщение: {message}\n")
+            with open(r"C:\Users\Amd\Desktop\SkyPro\Messages.txt", "a", encoding="utf-8") as file:
+                file.write(write_in_file)
+            # А здесь мы просто возвращаем простой ответ пользователю на сайте:
+            return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+        return render(request, 'catalog/contacts.html')
 
-def product_detail(request: HttpRequest, pk: int):
-    product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
-    return render(request, 'catalog/product_detail.html', context=context)
+# def show_home(request: HttpRequest):
+#     """Обрабатывает запрос и возвращает html-страницу"""
+#     if request.method == 'GET':
+#         products = Product.objects.all()
+#         context = {'products': products}
+#
+#         return render(request, "catalog/home.html", context=context)
+#
+#
+# def show_contacts(request: HttpRequest):
+#     """Обрабатывает запрос и возвращает html-страницу"""
+#     if request.method == 'GET':
+#         return render(request, "catalog/contacts.html")
+#
+#
+# def contacts(request: HttpRequest):
+#     """Обрабатываем форму и возвращаем ответ"""
+#     if request.method == 'POST':
+#         # Получение данных из формы
+#         name = request.POST.get('name')
+#         phone = request.POST.get('phone')
+#         message = request.POST.get('message')
+#         # Обработка данных (например, сохранение в БД, отправка email и т. д.)
+#         write_in_file = (f"\nИмя пользователя: {name}\n"
+#                          f"Телефон пользователя: {phone}\n"
+#                          f"Его сообщение: {message}\n")
+#         with open(r"C:\Users\Amd\Desktop\SkyPro\Messages.txt", "a", encoding="utf-8") as file:
+#             file.write(write_in_file)
+#         # А здесь мы просто возвращаем простой ответ пользователю на сайте:
+#         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+#     return render(request, 'catalog/contacts.html')
+#
+#
+# def product_detail(request: HttpRequest, pk: int):
+#     product = get_object_or_404(Product, pk=pk)
+#     context = {'product': product}
+#     return render(request, 'catalog/product_detail.html', context=context)
